50b408721c4d9891af85b50318e7443f67316ac3
Moses-odessa <moses2804@gmail.com>
2018-06-08 12:04:06 +0300

    add maven and retrifit2 dependency

--- src/ua/moses/converter/model/CurrencyConverter.java
+++
@@ -1,9 +0,0 @@
-package ua.moses.converter.model;
-
-import java.util.List;
-
-public interface CurrencyConverter {
-    double convert(double amount, String fromCurrency, String toCurrency);
-    List<CurrencyData> currencies();
-
-}
